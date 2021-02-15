c1a726bfa4c2e06f79ba50be15a2e66039d2af20
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-14 23:07:28 -0600

    Created game and question model with scaffold

---
+++ backend/db/migrate/20210215050242_create_questions.rb
@@ -0,0 +1,16 @@
+class CreateQuestions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :questions do |t|
+      t.belongs_to :game, null: false, foreign_key: true
+      t.string :difficulty
+      t.text :content
+      t.string :correct_answer
+      t.string :option_a
+      t.string :option_b
+      t.string :option_c
+      t.string :option_d
+
+      t.timestamps
+    end
+  end
+end
