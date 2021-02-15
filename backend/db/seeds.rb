c8574c8189b27eb77aa92c50039e979dafeccca6
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-14 23:13:58 -0600

    Adds seeded game difficulty

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Game.new(difficulty: "Beginner")
+Game.new(difficulty: "Intermediate")
+Game.new(difficulty: "Expert")
