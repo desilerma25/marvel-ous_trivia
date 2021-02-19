af80b68faafbd7c8c2485e42395ea2e9acb5196f
Desiree Lerma <desi.lerma96@yahoo.com>
2021-02-19 10:24:07 -0600

    Fixes bug in fetch POST req.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # resources :questions
   resources :games do
-    resources :questions, shallow: true
+    resources :questions, only: [:index]
   end
+
+  resources :questions, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
